d412ae72351705b10f57c1cd24d99394433dbb92
jmb2547 <johnmbryan.25@gmail.com>
2015-05-29 16:51:00 -0700

    Update BLB trial.R

--- BLB trial.R
+++ BLB trial.R
@@ -4,20 +4,21 @@ data <- rnorm(10000,0,1)
 #parameters 
 n <- length(data)
 b <- n^.5
-s <- 10
+s <- 100
 r <- 10000
 
 subsamples <- matrix(,nrow=s,ncol=trunc(b))
 subsamples.means <- c()
 quality.estimates <- matrix(,nrow=s,ncol=2)
 stats.matrix <- matrix(,nrow=s,ncol=r)
+
 for (i in 1:s) {
   subsamples[i,] <- sample(data, b,replace=F)
   subsamples.means[i] <- mean(subsamples[i,])
   stats <- c()
   for (j in 1:r) {
-    resample <- sample(subsamples[i,],b,replace=T)
-    stats[j] <- mean(resample)
+    resample.vector <- rmultinom(1,n,rep(1/b,b))
+    stats[j] <- sum(resample.vector*subsamples[i,])/n
   }
   stats.matrix[i,] <- stats
   quality.estimates[i,] <- quantile(stats,c(.025,.975))
@@ -35,11 +36,13 @@ for (i in 1:s) {
   print(abs(subsamples.means[i]-mean(stats.matrix[i,])))
 }
 
+
+
 #using the known sampling distribution of mean
 #redrawing random samples
 stats.known <- c()
 for (i in 1:r) {
-  data.test <- rnorm(100,0,1)
+  data.test <- rnorm(10000,0,1)
   stats.known[i] <- mean(data.test)
 }
 hist(stats.known)
@@ -47,9 +50,9 @@ quantile(stats.known,c(0.025,.975))
 
 #regular bootstrap with n=100
 stats.known <- c()
-data.test <- rnorm(100,0,1)
+data.test <- rnorm(10000,0,1)
 for (i in 1:r) {
-  data.boot <- sample(data.test,100,replace=T)
+  data.boot <- sample(data.test,10000,replace=T)
   stats.known[i] <- mean(data.boot)
 }
 hist(stats.known)
