152c36255be6dad288a4da4f4af95755373e0e72
Yenny Zhang <yennyszhang@gmail.com>
2015-06-02 15:51:52 -0700

    Update BLBfunction.R

--- BLBfunction.R
+++ BLBfunction.R
@@ -1,19 +1,19 @@
 BLB <- function(data,n,b,s,r) {
   
-#   num.cluster1 = 25
-#   num.runs1 = 1*num.cluster1
-#   
-#   run1 <- list()
-#   length(run1) <- s #need this val
-#   
-#   c1 <- makeCluster(num.cluster1)
-#   registerDoParallel(c1)
+  #   num.cluster1 = 25
+  #   num.runs1 = 1*num.cluster1
+  #   
+  #   run1 <- list()
+  #   length(run1) <- s #need this val
+  #   
+  #   c1 <- makeCluster(num.cluster1)
+  #   registerDoParallel(c1)
   
   subsamples <- matrix(,nrow=s,ncol=trunc(b))
   quality.estimates <- matrix(,nrow=s,ncol=7)
   stats.matrix <- matrix(,nrow=s,ncol=r)
   
-#   perm.results.sim <- foreach(i=1:s, .combine='rbind') %dopar%{
+  #   perm.results.sim <- foreach(i=1:s, .combine='rbind') %dopar%{
   for (i in 1:s) {
     subsamples[i,] <- sample(data,b,replace=F)
     stats <- c()
@@ -24,10 +24,6 @@ BLB <- function(data,n,b,s,r) {
     stats.matrix[i,] <- stats
     quality.estimates[i,] <- quantile(stats,c(0.005,.025,0.05,.5,.95,.975,.995))
   }
-    
-  final.quality.estimate <- c(mean(quality.estimates[,1]),mean(quality.estimates[,2]),mean(quality.estimates[,3]),
-                              mean(quality.estimates[,4]),mean(quality.estimates[,5]),mean(quality.estimates[,6]),
-                              mean(quality.estimates[,7]))
-
+  final.quality.estimate = apply(quality.estimates, 2, mean)
   return(final.quality.estimate)
 }
