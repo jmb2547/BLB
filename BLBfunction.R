c87bda2d0e52e9f9c8cc7325dded5d8f7c3c3b5e
jmb2547 <johnmbryan.25@gmail.com>
2015-06-03 11:23:04 -0700

    Update BLBfunction.R

--- BLBfunction.R
+++ BLBfunction.R
@@ -1,20 +1,19 @@
 BLB <- function(data,n,b,s,r) {
   
-  #   num.cluster1 = 25
-  #   num.runs1 = 1*num.cluster1
-  #   
-  #   run1 <- list()
-  #   length(run1) <- s #need this val
-  #   
-  #   c1 <- makeCluster(num.cluster1)
-  #   registerDoParallel(c1)
+  num.cluster1 = 25
+  num.runs1 = 4*num.cluster1
+    
+  run1 <- list()
+  length(run1) <- s #need this val
+    
+  c1 <- makeCluster(num.cluster1)
+  registerDoParallel(c1)
   
   subsamples <- matrix(,nrow=s,ncol=trunc(b))
   quality.estimates <- matrix(,nrow=s,ncol=7)
   stats.matrix <- matrix(,nrow=s,ncol=r)
   
-  #   perm.results.sim <- foreach(i=1:s, .combine='rbind') %dopar%{
-  for (i in 1:s) {
+  perm.results.sim <- foreach(i=1:s, .combine='rbind') %dopar%{
     subsamples[i,] <- sample(data,b,replace=F)
     stats <- c()
     for (j in 1:r) {
@@ -24,6 +23,6 @@ BLB <- function(data,n,b,s,r) {
     stats.matrix[i,] <- stats
     quality.estimates[i,] <- quantile(stats,c(0.005,.025,0.05,.5,.95,.975,.995))
   }
-  final.quality.estimate = apply(quality.estimates, 2, mean)
+  final.quality.estimate = apply(perm.results.sim, 2, mean)
   return(final.quality.estimate)
 }
