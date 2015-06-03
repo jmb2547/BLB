81962d13642a615cc083629a90ed1098b427b763
jmb2547 <johnmbryan.25@gmail.com>
2015-06-03 09:24:26 -0700

    Update BLBparallelized.R

--- BLBparallelized.R
+++ BLBparallelized.R
@@ -11,7 +11,7 @@ r <- 10000
 
 output.matrix <- matrix(,nrow=100,ncol=11)
 
-for (i in 1:10) {
+for (i in 1:100) {
   data <- rnorm(n,0,1)
   BLB.output <- BLB(data,n,b,s,r)
   output.matrix[i,1:7] <- BLB.output
@@ -26,6 +26,4 @@ output.matrix[,8:11] <- cbind(interval.length, caught.mu, below.mu, above.mu)
 colnames(output.matrix) <- c('0.005','0.025','0.05','0.5','0.95','0.975','0.995',
                              'Length','Caught mu','below mu','above mu')
 
-write.table(output.matrix,file='BLB_n10000_b100_s25_r10000',row.names=F,col.names=T,sep=",")
-
-
+write.table(output.matrix,file='BLB_n10000_b100_s100_r10000',row.names=F,col.names=T,sep=",")
